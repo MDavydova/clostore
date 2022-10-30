965f334c60f393284bea670652f14202793596f5
Milena Davydova <milena@yelk.com.ua>
2022-10-30 05:07:30 +0100

    feat: created signin component

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,30 @@
+import { Outlet } from 'react-router'
+import { Link } from 'react-router-dom'
+import { Fragment } from "react";
+import './navigation.styles.scss'
+import {ReactComponent as Logo} from '../../assets/crown.svg'
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to="/">
+                    <Logo className='logo'/>
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='/shop'>
+                        SHOP
+                    </Link>
+                </div>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='sign-in'>
+                        Sign In
+                    </Link>
+                </div>
+            </div>
+            <Outlet/>
+        </Fragment>
+    )
+}
+
+export default Navigation;
---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,47 @@
+import {initializeApp} from 'firebase/app'
+import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import {getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDAD_wyKPFy-GxM-Ob1M-2u5IvHXTov6rA",
+    authDomain: "capion-db.firebaseapp.com",
+    projectId: "capion-db",
+    storageBucket: "capion-db.appspot.com",
+    messagingSenderId: "929050947212",
+    appId: "1:929050947212:web:c8e2716e19124abcdc55a9"
+};
+
+const app = initializeApp(firebaseConfig)
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+})
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async(userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if(!userSnapshot.exists()) {
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+      } catch (error) {
+          console.log('error creating user', error.message)
+      }
+  }
+
+  return userDocRef;
+}
